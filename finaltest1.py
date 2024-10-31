9a9b6872f49ae19d3f9846a579d5fa095f964017
bemomcares <bmcs1231@gmail.com>
2024-10-31 23:48:48 +0800

    Update finaltest1.py

--- finaltest1.py
+++ finaltest1.py
@@ -388,7 +388,7 @@ def send_weekly_reminder():
 def weekly_reminder_schedule():
     """設置每周的指定時間推播"""
     print("weekly_reminder_schedule() has started.")
-    schedule.every().thursday.at("15:50").do(send_weekly_reminder)
+    schedule.every().thursday.at("15:56").do(send_weekly_reminder)
     
     while True:
         schedule.run_pending()
